1ff2a8a75f29ffca5a7858aacd9be4efb181324c
regenerativep <regeneralp@gmail.com>
2019-07-01 22:30:10 -0400

    created engine project
    created asset manager

---
+++ PlatformerEngine/PlatformerEngine/AssetManager.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformerEngine
+{
+    public static class AssetManager
+    {
+        private static Dictionary<string, Texture2D> textureAssets = new Dictionary<string, Texture2D>();
+        private static Dictionary<string, Texture2D[]> framedTextureAssets = new Dictionary<string, Texture2D[]>();
+        private static Dictionary<string, SoundEffect> soundAssets = new Dictionary<string, SoundEffect>();
+        private static List<KeyValuePair<string, Action<Texture2D>>> textureAssetRequests = new List<KeyValuePair<string, Action<Texture2D>>>();
+        private static List<KeyValuePair<string, Action<Texture2D[]>>> framedTextureAssetRequests = new List<KeyValuePair<string, Action<Texture2D[]>>>();
+        private static List<KeyValuePair<string, Action<SoundEffect>>> soundAssetRequests = new List<KeyValuePair<string, Action<SoundEffect>>>();
+        public static ContentManager Content;
+        public static Texture2D[] LoadFramedTexture(string internalName, string location, int frameCount)
+        {
+            Texture2D[] frames = new Texture2D[frameCount];
+            for (int i = 0; i < frames.Length; i++)
+            {
+                frames[i] = Content.Load<Texture2D>(location + i.ToString());
+            }
+            foreach (KeyValuePair<string, Action<Texture2D[]>> req in framedTextureAssetRequests)
+            {
+                if (req.Key.Equals(internalName))
+                {
+                    req.Value.Invoke(frames);
+                    framedTextureAssetRequests.Remove(req);
+                }
+            }
+            framedTextureAssets[internalName] = frames;
+            return frames;
+        }
+        public static Texture2D LoadTexture(string internalName, string location)
+        {
+            Texture2D texture = Content.Load<Texture2D>(location);
+            foreach (KeyValuePair<string, Action<Texture2D>> req in textureAssetRequests)
+            {
+                if (req.Key.Equals(internalName))
+                {
+                    req.Value.Invoke(texture);
+                    textureAssetRequests.Remove(req);
+                }
+            }
+            textureAssets[internalName] = texture;
+            return texture;
+        }
+        public static SoundEffect LoadSound(string internalName, string location)
+        {
+            SoundEffect sound = Content.Load<SoundEffect>(location);
+            foreach (KeyValuePair<string, Action<SoundEffect>> req in soundAssetRequests)
+            {
+                if (req.Key.Equals(internalName))
+                {
+                    req.Value.Invoke(sound);
+                    soundAssetRequests.Remove(req);
+                }
+            }
+            soundAssets[internalName] = sound;
+            return sound;
+        }
+        public static void RequestFramedTexture(string assetName, Action<Texture2D[]> callback)
+        {
+            if(framedTextureAssets.ContainsKey(assetName))
+            {
+                callback.Invoke(framedTextureAssets[assetName]);
+            }
+            else
+            {
+                framedTextureAssetRequests.Add(new KeyValuePair<string, Action<Texture2D[]>>(assetName, callback));
+            }
+        }
+        public static void RequestTexture(string assetName, Action<Texture2D> callback)
+        {
+            if (textureAssets.ContainsKey(assetName))
+            {
+                callback.Invoke(textureAssets[assetName]);
+            }
+            else
+            {
+                textureAssetRequests.Add(new KeyValuePair<string, Action<Texture2D>>(assetName, callback));
+            }
+        }
+        public static void RequestSound(string assetName, Action<SoundEffect> callback)
+        {
+            if (soundAssets.ContainsKey(assetName))
+            {
+                callback.Invoke(soundAssets[assetName]);
+            }
+            else
+            {
+                soundAssetRequests.Add(new KeyValuePair<string, Action<SoundEffect>>(assetName, callback));
+            }
+        }
+    }
+}
---
+++ PlatformerEngine/PlatformerEngine/PlatformerGame.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlatformerEngine
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class PlatformerGame : Game
+    {
+        public GraphicsDeviceManager Graphics;
+        private SpriteBatch spriteBatch;
+
+        public PlatformerGame()
+        {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            ChangeResolution(1024, 768);
+            base.Initialize();
+        }
+        public void SetFullscreen(bool full)
+        {
+            Graphics.IsFullScreen = full;
+            Graphics.ApplyChanges();
+        }
+        public void ChangeResolution(int width, int height)
+        {
+            Graphics.PreferredBackBufferWidth = 1024;
+            Graphics.PreferredBackBufferHeight = 768;
+            Graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
